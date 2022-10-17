66059c49a85b70a33edd0e1493ada7102ecab32e
DevelPlayz321 <88618929+DevelPlayz321@users.noreply.github.com>
2022-10-17 13:15:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(500, 550);
+    canvas = createCanvas(560, 560);
+    canvas.position(560, 150);
+    model = ml5.poseNet(video, modelLoaded);
+    model.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("Model loaded successfully");
+}
+
+function gotPoses(results){
+    if(results){
+        console.log(results);
+    }
+}
